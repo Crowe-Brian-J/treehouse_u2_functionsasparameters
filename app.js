4b5fb6f89013720715e44ff81b92b51328d66090
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-09 22:45:05 -0400

    add remove button to each li

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const btnToggle = document.querySelector('.btn-toggle')
 const btnRemove = document.querySelector('.btn-remove')
 const taskList = document.querySelector('.list-container ul')
 const input = document.querySelector('.input-main')
+const listItems = taskList.children
+
+//attach remove button to li
+const attachRemoveButton = (li) => {
+  let remove = document.createElement('button')
+  remove.className = 'remove'
+  remove.textContent = 'Remove'
+  li.appendChild(remove)
+}
+
+for (let i = 0; i < listItems.length; i++) {
+  attachRemoveButton(listItems[i])
+}
 
 //target taskList instead of listItems so we don't need to iterate over them, and each item responds as expected
-taskList.addEventListener('mouseover', (evt) => {
+/* taskList.addEventListener('mouseover', (evt) => {
   if (evt.target.tagName === 'LI') {
     evt.target.textContent = evt.target.textContent.toUpperCase()
   }
-})
+}) */
 
 btnToggle.addEventListener('click', () => {
   const listContainer = document.querySelector('.list-container')
@@ -24,7 +37,12 @@ btnToggle.addEventListener('click', () => {
 
 btnCreate.addEventListener('click', () => {
   const list = document.querySelector('ul')
-  list.insertAdjacentHTML('afterbegin', `<li>${input.value}</i>`)
+  const li = document.createElement('li')
+  li.textContent = input.value
+  list.insertAdjacentHTML(
+    'afterbegin',
+    `<li>${input.value}<button class="remove">Remove</button></li>`
+  )
   input.value = ''
 })
 
